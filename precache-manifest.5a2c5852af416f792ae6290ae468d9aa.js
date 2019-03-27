ef81d6a4cae65c2ef09be59b6c97ba3ab0123ddd
giseleml <giselemaraleo@gmail.com>
2019-03-27 13:39:24 -0300

    Introducing Routes

---
+++ precache-manifest.5a2c5852af416f792ae6290ae468d9aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "858d47b2aac3780d5fcc",
+    "url": "./static/js/main.858d47b2.chunk.js"
+  },
+  {
+    "revision": "a619d5026dd0d7bf6512",
+    "url": "./static/js/1.a619d502.chunk.js"
+  },
+  {
+    "revision": "858d47b2aac3780d5fcc",
+    "url": "./static/css/main.051d7f9b.chunk.css"
+  },
+  {
+    "revision": "f0e1e6bd9b72b18243172a307725d9c3",
+    "url": "./index.html"
+  }
+];
