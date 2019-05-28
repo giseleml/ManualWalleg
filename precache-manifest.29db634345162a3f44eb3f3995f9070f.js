0d5acc348177bd348be0408fdbb197f0f7377d78
giseleml <giselemaraleo@gmail.com>
2019-05-28 12:58:50 -0300

    Refactored Build

---
+++ precache-manifest.29db634345162a3f44eb3f3995f9070f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b2d2a43889f68c523458",
+    "url": "./static/js/main.b2d2a438.chunk.js"
+  },
+  {
+    "revision": "4d78c8842f6a3b0177ea",
+    "url": "./static/js/1.4d78c884.chunk.js"
+  },
+  {
+    "revision": "b2d2a43889f68c523458",
+    "url": "./static/css/main.09b91568.chunk.css"
+  },
+  {
+    "revision": "b72700f92e06c7252e4fade03359736f",
+    "url": "./index.html"
+  }
+];
