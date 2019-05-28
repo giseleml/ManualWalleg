ed1ec35d361a29d02f99c53edb9ed970a666c069
giseleml <giselemaraleo@gmail.com>
2019-05-28 16:45:12 -0300

    BrowserRouter changes

---
+++ precache-manifest.4a3c79be22e64091baf1bbbf0d96e2e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "641b4182d13ef21e0ce7",
+    "url": "/ManualWalleg/static/js/runtime~main.641b4182.js"
+  },
+  {
+    "revision": "f5245c38f75363bb0c07",
+    "url": "/ManualWalleg/static/js/main.f5245c38.chunk.js"
+  },
+  {
+    "revision": "4d78c8842f6a3b0177ea",
+    "url": "/ManualWalleg/static/js/1.4d78c884.chunk.js"
+  },
+  {
+    "revision": "f5245c38f75363bb0c07",
+    "url": "/ManualWalleg/static/css/main.09b91568.chunk.css"
+  },
+  {
+    "revision": "6a8bda91364d590134dccf2b7248ffc1",
+    "url": "/ManualWalleg/index.html"
+  }
+];
