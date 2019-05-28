56df93e101be436e9b222b5609d77f0202194efe
giseleml <giselemaraleo@gmail.com>
2019-05-28 16:51:59 -0300

    Updating BrowserRouter to HashRouter

---
+++ precache-manifest.d291a1ab064f5492e9cef3076ab66f74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "641b4182d13ef21e0ce7",
+    "url": "/ManualWalleg/static/js/runtime~main.641b4182.js"
+  },
+  {
+    "revision": "013ddf49fd48fe0bf697",
+    "url": "/ManualWalleg/static/js/main.013ddf49.chunk.js"
+  },
+  {
+    "revision": "682a1eda9b8221f80fc7",
+    "url": "/ManualWalleg/static/js/1.682a1eda.chunk.js"
+  },
+  {
+    "revision": "013ddf49fd48fe0bf697",
+    "url": "/ManualWalleg/static/css/main.09b91568.chunk.css"
+  },
+  {
+    "revision": "01b289d00d08b8c529908a037418a395",
+    "url": "/ManualWalleg/index.html"
+  }
+];
