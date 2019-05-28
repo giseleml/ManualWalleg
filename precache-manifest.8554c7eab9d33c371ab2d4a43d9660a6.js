ab0fbdab931035e552849efe3d7c41c95247de54
giseleml <giselemaraleo@gmail.com>
2019-05-28 16:38:08 -0300

    Fixing React Router

---
+++ precache-manifest.8554c7eab9d33c371ab2d4a43d9660a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "641b4182d13ef21e0ce7",
+    "url": "/ManualWalleg/static/js/runtime~main.641b4182.js"
+  },
+  {
+    "revision": "36c14639d38f3bee819b",
+    "url": "/ManualWalleg/static/js/main.36c14639.chunk.js"
+  },
+  {
+    "revision": "4d78c8842f6a3b0177ea",
+    "url": "/ManualWalleg/static/js/1.4d78c884.chunk.js"
+  },
+  {
+    "revision": "36c14639d38f3bee819b",
+    "url": "/ManualWalleg/static/css/main.09b91568.chunk.css"
+  },
+  {
+    "revision": "0345187a47159755109dc17231969fe6",
+    "url": "/ManualWalleg/index.html"
+  }
+];
