e8d5dd41ee183d1319d45e8f41e9a48781d3c7aa
giseleml <giselemaraleo@gmail.com>
2020-01-21 18:14:40 -0300

    Fixing sidebar

--- src/components/side-nav.js
+++ src/components/side-nav.js
@@ -1,37 +1,22 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+import linksToRoute from './links.js'
+
 export class SideNav extends React.Component {
   render() {
     return(
-      <div>
-      <div className="side-nav-info">
-        <img className="logo-walleg" src="https://1.bp.blogspot.com/--8K7QPoVvdg/XO1ZmPR_l4I/AAAAAAAAA3Q/gu1RRp4YDGAvkl1H1NBSbSNS_gKHQ7MQgCLcBGAs/s1600/walleg-logo.png" alt = "Logo Walleg"/>
-      </div>
       <div className="side-nav">
-        <Link to="/">Login e Início</Link>
-        <Link to="/tela-inicial">Tela de Início</Link>
-        <Link to="/cadastros">Cadastros</Link>
-        <Link to="/bancos">Bancos</Link>
-        <Link to="/clientes">Clientes</Link>
-        <Link to="/fornecedores">Fornecedores</Link>
-        <Link to="/funcionarios">Funcionários</Link>
-        <Link to="/produtos">Produtos</Link>
-        <Link to="/estoque">Estoque</Link>
-        <Link to="/retira">Retira</Link>
-        <Link to="/kits">Catálogo de Kits</Link>
-        <Link to="/transportadoras">Transportadoras</Link>
-        <Link to="/caixa">Caixa</Link>
-        <Link to="/compras">Compras</Link>
-        <Link to="/contas">Contas</Link>
-        <Link to="orcamentos">Orçamentos</Link>
-        <Link to="/pedido-de-venda">Pedido de Venda</Link>
-        <Link to="/nfs-e">Nota Fiscal de Serviço (NFS-e)</Link>
-        <Link to="/nfs">Nota Fiscal de Saída</Link>
-        <Link to="/nota-de-entrada">Nota Fiscal de Entrada</Link>
-        <Link to="/relatorio">Relatório de Movimento</Link>
-      </div>
+        <div className="side-nav-info">
+          <img className="logo-walleg" src="https://1.bp.blogspot.com/--8K7QPoVvdg/XO1ZmPR_l4I/AAAAAAAAA3Q/gu1RRp4YDGAvkl1H1NBSbSNS_gKHQ7MQgCLcBGAs/s1600/walleg-logo.png" alt = "Logo Walleg"/>
+        </div>
+        <div className="side-nav-menu">
+          {
+            linksToRoute.map(link => <Link to={link.url}>{link.title}</Link>)
+          }
+        </div>
      </div>
     )
   }
 }
+
