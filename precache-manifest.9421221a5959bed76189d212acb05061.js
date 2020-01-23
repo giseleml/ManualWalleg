83e9fa1570e4f4fa581f3048d594fcd9ec9e7786
giseleml <giselemaraleo@gmail.com>
2020-01-23 11:22:41 -0300

    Mobile sidenav

---
+++ precache-manifest.9421221a5959bed76189d212acb05061.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b21e2e273edca9eadb21",
+    "url": "/ManualWalleg/static/css/main.f873bba9.chunk.css"
+  },
+  {
+    "revision": "b21e2e273edca9eadb21",
+    "url": "/ManualWalleg/static/js/main.84379ccb.chunk.js"
+  },
+  {
+    "revision": "22668aeb2a227a63a1e3",
+    "url": "/ManualWalleg/static/js/runtime~main.db807e18.js"
+  },
+  {
+    "revision": "24fb10a537eed4d93280",
+    "url": "/ManualWalleg/static/js/2.82668c50.chunk.js"
+  },
+  {
+    "revision": "1cefc6e98b1c08b37244783df44cf834",
+    "url": "/ManualWalleg/index.html"
+  }
+];
--- src/components/side-nav.js
+++ src/components/side-nav.js
@@ -7,6 +7,12 @@ export class SideNav extends React.Component {
   render() {
     return(
       <div className="side-nav">
+        <input type="checkbox" id="hamb-btn"/>
+          <label htmlFor="hamb-btn">
+              <span></span>
+              <span></span>
+              <span></span>
+          </label>
         <div className="side-nav-info">
           <img className="logo-walleg" src="https://1.bp.blogspot.com/--8K7QPoVvdg/XO1ZmPR_l4I/AAAAAAAAA3Q/gu1RRp4YDGAvkl1H1NBSbSNS_gKHQ7MQgCLcBGAs/s1600/walleg-logo.png" alt = "Logo Walleg"/>
         </div>
