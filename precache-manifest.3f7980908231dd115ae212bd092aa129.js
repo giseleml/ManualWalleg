b71f25b60e73bcc71e0a274e9b85d4e97e79dc49
giseleml <giselemaraleo@gmail.com>
2020-01-23 11:28:19 -0300

    CSS files

---
+++ precache-manifest.3f7980908231dd115ae212bd092aa129.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fd492e572a87cce8692",
+    "url": "/ManualWalleg/static/css/main.c7746ae0.chunk.css"
+  },
+  {
+    "revision": "9fd492e572a87cce8692",
+    "url": "/ManualWalleg/static/js/main.6b4c3a86.chunk.js"
+  },
+  {
+    "revision": "22668aeb2a227a63a1e3",
+    "url": "/ManualWalleg/static/js/runtime~main.db807e18.js"
+  },
+  {
+    "revision": "24fb10a537eed4d93280",
+    "url": "/ManualWalleg/static/js/2.82668c50.chunk.js"
+  },
+  {
+    "revision": "45bccefe2da131aabed271f2b2523b17",
+    "url": "/ManualWalleg/index.html"
+  }
+];
