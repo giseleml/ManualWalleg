e5c840a6e53c45d88f8263c8a3ddade98d0b922b
giseleml <giselemaraleo@gmail.com>
2020-01-28 17:57:30 -0300

    Sidebar changes

--- src/components/side-nav.js
+++ src/components/side-nav.js
@@ -13,9 +13,6 @@ export class SideNav extends React.Component {
               <span></span>
               <span></span>
           </label>
-        <div className="side-nav-info">
-          <img className="logo-walleg" src="https://1.bp.blogspot.com/--8K7QPoVvdg/XO1ZmPR_l4I/AAAAAAAAA3Q/gu1RRp4YDGAvkl1H1NBSbSNS_gKHQ7MQgCLcBGAs/s1600/walleg-logo.png" alt = "Logo Walleg"/>
-        </div>
         <div className="side-nav-menu">
           {
             linksToRoute.map((link, i) => <Link to={link.url} key={i}>{link.title}</Link>)
